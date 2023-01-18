a16e2ccbd90610546ea8850a0ff4a82ba03e6223
Susanna <susanna.tuomi@myy.haaga-helia.fi>
2023-01-18 14:24:16 +0200

    Demo tehty

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
+import { useState } from 'react';
+import {Image} from 'react-native';
+
+
 
 export default function App() {
+
+const [text, setText] = useState('')
+
+const pressed = () => {
+  Alert.alert('You typed' + text)
+};
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image style={styles.image} source={{uri: 'https://www.sttinfo.fi/data/images/00304/be7db042-6b61-49f9-9bcd-7fd41b7bc35d.jpg'}} />
+     <TextInput
+     style={styles.input}
+     onChangeText={text => setText(text)}
+     value={text}
+     />
+      <Button title="Press me" onPress={pressed}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +34,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image:{ width:200, 
+    height: 100
+  },
+  input:{ width:200, 
+    borderColor:'grey', 
+    borderWidth: 1
+  },
+
 });
